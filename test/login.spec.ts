3f7d4505c6db6d4d301fbcbfe81ed303f51f3f21
AbinayaArul <abinaya.palani@a-cti.com>
2020-06-30 22:01:46 +0530

    trying in version 5 WDIO

---
+++ test/login.spec.ts
@@ -0,0 +1,11 @@
+import {expect} from 'chai';
+import LoginPage from '../src/pages/LoginPage';
+
+describe('Login page', () => {
+    it('should allow access with correct credentials', () => {
+        LoginPage.open;
+        LoginPage.loginWithCredentials('tomsmith', 'SuperSecretPassword!');
+        
+        expect(LoginPage.flash).to.include('You logged into a secure area!');
+    });
+});
