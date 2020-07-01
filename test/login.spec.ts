eabdbc33aee78bb7f74e20ec759ee0c394475c8c
AbinayaArul <abinaya.palani@a-cti.com>
2020-07-01 10:59:03 +0530

    add title result

--- test/login.spec.ts
+++ test/login.spec.ts
@@ -5,6 +5,8 @@ import LoginPage from '../src/pages/LoginPage';
 describe('Login page', () => {
     it('should allow access with correct credentials', () => {
         LoginPage.open;
+        console.log(browser.getTitle())
+
         LoginPage.loginWithCredentials('tomsmith', 'SuperSecretPassword!');
         
         expect(LoginPage.flash).to.include('You logged into a secure area!');
